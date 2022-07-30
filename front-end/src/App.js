fa122ace1dbd8ca5c48fdd251a98a3b91219af4e
Robert Chen <wiroger9595@gmail.com>
2022-07-30 23:42:31 +0800

    update detail page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 //import data from "../../back-end/data";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
@@ -8,17 +11,28 @@ import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">coco online</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>coco online</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
 
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
